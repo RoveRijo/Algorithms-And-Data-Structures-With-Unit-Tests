df41373af00f9a58804ad0d6b12499acb0841d6c
Rijo <rijo@udhaar.co>
2024-03-06 18:04:32 +0530

    [Fix] Added quick sort algorithm with test cases

--- app/src/test/java/com/example/algorithmsanddatastructureswithtests/ExampleUnitTest.kt
+++ app/src/test/java/com/example/algorithmsanddatastructureswithtests/ExampleUnitTest.kt
@@ -27,3 +27,20 @@ class BubbleSortTests {
     }
 
 }
+class QuickSortTests{
+    @Test
+    fun quickSortTestCase1(){
+        // Testing Array [3,0,4,5,87,45,1]
+        val arrayIn= arrayOf(3,0,4,5,87,45,1)
+        val output = quickSort(arrayIn)
+        assertArrayEquals(output,arrayOf(0,1,3,4,5,45,87))
+    }
+    @Test
+    fun quickSortTestCase2(){
+        // Testing Array [2,4,5,4,87,516,5,4,54,878,78,75,54,5,6,46545,45,75,15,4,4,0,6,5,66]
+        val arrayIn= arrayOf(2,4,5,4,87,516,5,4,54,878,78,75,54,5,6,46545,45,75,15,4,4,0,6,5,66)
+        val output = quickSort(arrayIn)
+        assertArrayEquals(output,arrayOf(0,2,4,4,4,4,4,5,5,5,5,6,6,15,45,54,54,66,75,75,78,87,516,878,46545))
+    }
+
+}

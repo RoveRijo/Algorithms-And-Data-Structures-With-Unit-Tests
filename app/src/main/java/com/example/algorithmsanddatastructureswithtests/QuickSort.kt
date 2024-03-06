08f37a65d759d43d57c85101ac507f8e4bbd45d3
Rijo <rijo@udhaar.co>
2024-03-06 18:02:13 +0530

    [Fix] Added quick sort algorithm with test cases

--- app/src/main/java/com/example/algorithmsanddatastructureswithtests/QuickSort.kt
+++ app/src/main/java/com/example/algorithmsanddatastructureswithtests/QuickSort.kt
@@ -1,4 +1,34 @@
 package com.example.algorithmsanddatastructureswithtests
 
-class QuickSort {
+fun quickSort(arrayIn: Array<Int>): Array<Int> {
+    fun swap(pos1: Int, pos2: Int) {
+        val temp = arrayIn[pos1]
+        arrayIn[pos1] = arrayIn[pos2]
+        arrayIn[pos2] = temp
+    }
+
+    fun partition(low: Int, high: Int): Int {
+        val pivot = arrayIn[high]
+        var i = low - 1
+        for (j in low until high) {
+            if (arrayIn[j] <= pivot) {
+                i++
+                swap(i, j)
+            }
+        }
+        swap(i + 1, high)
+        return i + 1
+    }
+
+    fun sort(low: Int, high: Int) {
+        if(low<high) {
+            val pivotIndex = partition(low, high)
+            sort(low, pivotIndex - 1)
+            sort(pivotIndex + 1, high)
+        }
+    }
+
+
+    sort(0, arrayIn.size - 1)
+    return arrayIn
 }
